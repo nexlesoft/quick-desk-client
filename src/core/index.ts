817caf6d55f7203e930b1c9709d11757eec6ecbc
nhandt <nhandt@nexlesoft.com>
2024-09-12 11:40:34 +0700

    [81006] feat: export missing APIs

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,7 @@
+export * from "./activityLogApi";
 export * from "./axios";
+export * from "./mediaApi";
 export * from "./ticketApi";
+export * from "./ticketSettingStatusApi";
+export * from "./ticketSettingTypeApi";
 export * from "./types";
